ea0dcfd2e026724e9e0b6af5ddd0b7201406d6db
Sebastian Barreto <sebastian.e.barreto@gmail.com>
2021-06-24 15:42:56 -0300

    Add git information actuator
    https://www.baeldung.com/spring-git-information

--- src/main/java/com/calevin/springbootgcp/SpringBootGoogleCloudPlatformApplication.java
+++ src/main/java/com/calevin/springbootgcp/SpringBootGoogleCloudPlatformApplication.java
@@ -2,6 +2,9 @@ package com.calevin.springbootgcp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
 public class SpringBootGoogleCloudPlatformApplication {
@@ -10,4 +13,13 @@ public class SpringBootGoogleCloudPlatformApplication {
 		SpringApplication.run(SpringBootGoogleCloudPlatformApplication.class, args);
 	}
 
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+		PropertySourcesPlaceholderConfigurer propsConfig
+				= new PropertySourcesPlaceholderConfigurer();
+		propsConfig.setLocation(new ClassPathResource("git.properties"));
+		propsConfig.setIgnoreResourceNotFound(true);
+		propsConfig.setIgnoreUnresolvablePlaceholders(true);
+		return propsConfig;
+	}
 }
