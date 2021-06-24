43c4df73b8c6e65d2f94839d1ba4d8504aefc3a7
Sebastian Barreto <sebastian.e.barreto@gmail.com>
2021-06-24 04:10:22 -0300

    Inicializacion proyecto Spring Boot

---
+++ src/main/java/com/calevin/springbootgcp/GreetingController.java
@@ -0,0 +1,22 @@
+package com.calevin.springbootgcp;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s! (%s)";
+    private final AtomicLong counter = new AtomicLong();
+    @Value("${envVar:local}")
+    private String envVar;
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name, this.envVar));
+    }
+}
---
+++ src/test/java/com/calevin/springbootgcp/SpringBootGoogleCloudPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calevin.springbootgcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGoogleCloudPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
