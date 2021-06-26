2968097cd962c0364464f0edd5b484f7d03a0c0b
Sebastian Barreto <sebastian.e.barreto@gmail.com>
2021-06-26 02:36:01 -0300

    Mejora manejo error
    Uso de ResponseEntityExceptionHandler para manejar todas las excepciones

--- src/main/java/com/calevin/springbootgcp/errores/GlobalControllerAdvice.java
+++ src/main/java/com/calevin/springbootgcp/errores/GlobalControllerAdvice.java
@@ -1,13 +1,16 @@
 package com.calevin.springbootgcp.errores;
 
 import com.fasterxml.jackson.databind.JsonMappingException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class GlobalControllerAdvice {
+public class GlobalControllerAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ApiError> handleRegistroNoEncontrado(NotFoundException ex) {
@@ -17,11 +20,10 @@ public class GlobalControllerAdvice {
                 .body(new ApiError(HttpStatus.NOT_FOUND, ex.getMessage()));
     }
 
-    @ExceptionHandler(JsonMappingException.class)
-    public ResponseEntity<ApiError> handleJsonMappingException(JsonMappingException ex) {
-
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(new ApiError(HttpStatus.BAD_REQUEST, ex.getMessage()));
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+                                                             HttpStatus status, WebRequest request) {
+        ApiError apiError = new ApiError(status, ex.getMessage());
+        return ResponseEntity.status(status).headers(headers).body(apiError);
     }
 }
